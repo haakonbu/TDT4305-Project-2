b67bad24cdd3d0fcbecbc399b2a3f0aa497b41a8
Håkon Pelsholen Busterud <haakonbusterud@gmail.com>
2020-03-26 15:04:53 +0100

    Tokenize and filter out punctuation

--- most_positive_reviews.py
+++ most_positive_reviews.py
@@ -1,5 +1,6 @@
 from pyspark import SparkContext, SparkConf
 import base64
+import re
 
 sparkConf = SparkConf().setAppName("Yelp").setMaster("local")
 sc = SparkContext(conf=sparkConf)
@@ -17,6 +18,24 @@ header = rdd.first()
 rdd = rdd.filter(lambda line: line != header)
 
 
-rdd = rdd.map(lambda fields: [fields[2], base64.b64decode(fields[3]).lower()])
-tokenize = rdd.flatMap(lambda line: [line[0], line[1].split(" ")])
+def lower_clean_string(string):
+    """
+    Function used to make string lowercase filter out punctuation
 
+    :param string: Text string to strip clean and make lower case.
+    :return:
+    """
+
+    punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~[\n]'
+    lowercase_string = string.lower()
+    for character in punctuation:
+        lowercase_string = lowercase_string.replace(character, '')
+    return lowercase_string
+
+
+rdd = rdd.map(lambda fields: [fields[2], lower_clean_string(base64.b64decode(fields[3]))])
+tokens = rdd.map(lambda line: [line[0], line[1].split(" ")])    # Tokenize the reviews
+tokens = tokens.map(lambda x: [x[0], filter(None, x[1])])       # Filter out empty strings
+
+for i in tokens.take(10):
+    print(i)
