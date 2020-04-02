c0121f48c74c6033168e81432d964e4c46cb65f0
akselmadslien <aksel.madslien@teknologiporten.no>
2020-04-02 10:07:56 +0200

    attempt on fixing scores + alt. method

--- most_positive_reviews.py
+++ most_positive_reviews.py
@@ -41,8 +41,32 @@ tokens = tokens.map(lambda x: [x[0], filter(None, x[1])])       # Filter out emp
 file = open('stopwords.txt', 'r')
 stop_words = file.read().split('\n')
 
-# tokens = tokens.map(lambda x: [x[0], x[1].filter(lambda y: y not in stop_words)])
-# Need to fix line above
+tokens = tokens.map(lambda x: [x[0], (sc.parallelize(x[1])).filter(lambda y: y not in stop_words)])
+# Need to fix line above. Tried to make an rdd of the second column with sc.parallelize
 
 for i in tokens.take(2):
     print(i)
+
+
+#load AFINN-111 into dictionary as described in README
+afinn = dict(map(lambda (k,v): (k,int(v)),
+                     [ line.split('\t') for line in open("AFINN-111.txt") ]))
+
+
+# Example on how to use: sum(map(lambda word: afinn.get(word, 0), "Rainy day but still in a good mood".lower().split()))
+
+scores = tokens.map(lambda rev: [rev[0], sum(map(lambda word: afinn.get(word, 0), rev[1]))])
+
+
+
+# ALT 2. comment out from line 35 to use this
+# use flatmap for removing stop words
+"""
+rdd = rdd.map(lambda fields: [fields[2], lower_clean_string(base64.b64decode(fields[3])), 'linesplitter'])
+rdd = rdd.flatMap(lambda line: line.split(" "))
+filtered = rdd.filter(lambda y: y not in stop_words)
+mapped = filtered.map(lambda line: line.split('linesplitter'))
+
+scores = tokens.map(lambda rev: [rev[0], sum(map(lambda word: afinn.get(word, 0), rev[1:]))])
+
+"""
